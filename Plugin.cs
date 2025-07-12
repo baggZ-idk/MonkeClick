ad83b54fbe50b1e0d6e5732640910848d37038b8
cdubhenmorelikeCUBEHEN <cdubhen@gmail.com>
2025-07-12 07:24:30 -0400

    gorilla info watch
    1. switched over to gorilla info watch
    2. added left hand/right hand toggle
    3. added a few more colors
    
    creds to Zlothy for making most of the update

--- Plugin.cs
+++ Plugin.cs
@@ -1,83 +1,30 @@
 ﻿using System.Text;
+using System.Collections.Generic;
+using System;
 using BepInEx;
+using BepInEx.Bootstrap;
 using GorillaLocomotion;
 using UnityEngine;
-using BananaOS.Pages;
-using BananaOS;
-using System;
-using System.Collections.Generic;
-
-//Yes, I'm an American I spell it "color" not "colour" go cry about it.
+using GorillaInfoWatch.Attributes;
+using GorillaInfoWatch.Models;
+using GorillaInfoWatch.Models.Widgets;
+using System.Linq;
+using Photon.Pun;
 
 namespace MonkeClick
 {
-        [BepInPlugin("baggz.monkeclick", "MonkeClick", "1.0.0")]
-        public class Plugin : BaseUnityPlugin
-        {
-            bool inRoom;
-            public static LineRenderer lineRenderer;
-
-
-            void Start()
-            {
-                GorillaTagger.OnPlayerSpawned(OnGameInitialized);
-            }
-
-        void OnGameInitialized()
-        {
-            GameObject lineObj = new GameObject("RaycastLine");
-            lineRenderer = lineObj.AddComponent<LineRenderer>();
-
-            lineRenderer.positionCount = 2;
-            lineRenderer.startWidth = 0.01f;
-            lineRenderer.endWidth = 0.01f;
-            lineRenderer.material = new Material(Shader.Find("Sprites/Default"));
-            lineRenderer.enabled = false;
-        }
-
-        public static void RefreshColor(Color newColor, LineRenderer line, int newColorIndex)
-        {
-            line.startColor = newColor;
-            line.endColor = newColor;
-        }
-
-            void FixedUpdate()
-            {
-                lineRenderer.SetPosition(0, GTPlayer.Instance.rightControllerTransform.position);
-
-                if (ControllerInputPoller.instance.rightGrab && Page.active)
-                {
-                    lineRenderer.enabled = true;
-                    
-                    var hits = Physics.RaycastAll(GTPlayer.Instance.rightControllerTransform.position, GTPlayer.Instance.rightControllerTransform.forward, Mathf.Infinity, ~(1 << LayerMask.NameToLayer("Gorilla Boundary")));
-
-                    System.Array.Sort(hits, (a, b) => a.distance.CompareTo(b.distance));
-
-                    foreach (var hit in hits)
-                    {
-                            lineRenderer.enabled = true;
-                            lineRenderer.SetPosition(1, hit.point);
-                            if (ControllerInputPoller.instance.rightControllerIndexFloat > 0.5f && hit.transform.gameObject.layer == 18)
-                            {
-                                GorillaTagger.Instance.rightHandTriggerCollider.transform.position = hit.point;
-                                break;
-                            }
-                    }
-                }
-                else
-                {
-                    lineRenderer.enabled = false;
-                }
-            }
-        }
-
-    public class Page : WatchPage
+    [BepInPlugin(PluginInfo.GUID, PluginInfo.Name, PluginInfo.Version)]
+    [BepInDependency("dev.gorillainfowatch", BepInDependency.DependencyFlags.SoftDependency)]
+    public class Plugin : BaseUnityPlugin
     {
-        public override string Title => "Monke Click";
+        public static bool useRightHand = true; 
+        public static LineRenderer lineRenderer;
         public static bool active = false;
         public static int colorIndex = 0;
         public static List<Tuple<string, Color>> colors = new List<Tuple<string, Color>>
+
         {
+            
             Tuple.Create("white", Color.white),
             Tuple.Create("red", Color.red),
             Tuple.Create("green", Color.green),
@@ -85,56 +32,147 @@ namespace MonkeClick
             Tuple.Create("yellow", Color.yellow),
             Tuple.Create("cyan", Color.cyan),
             Tuple.Create("magenta", Color.magenta),
-            Tuple.Create("black", Color.black)
-            //if you want more colors make a pull request with the colors you want.
+            Tuple.Create("black", Color.black),
+            Tuple.Create("purple", new Color(0.5f, 0f, 0.5f)),
+            Tuple.Create("orange", new Color(1f, 0.5f, 0f)),
+            Tuple.Create("pink", new Color(1f, 0.75f, 0.8f)),
+            Tuple.Create("brown", new Color(0.6f, 0.3f, 0f)),
+            Tuple.Create("gray", Color.gray),
         };
-            public override bool DisplayOnMainMenu => true;
 
-            public override void OnPostModSetup()
+        void Start()
+        {
+            bool hasInfo = Chainloader.PluginInfos.ContainsKey("dev.gorillainfowatch");
+
+            if (!hasInfo)
             {
-                selectionHandler.maxIndex = 1;
+                Logger.LogError($"MonkeClick requires GorillaInfoWatch.");
+                this.enabled = false;
+                return;
             }
 
-            public override string OnGetScreenContent()
+            GorillaTagger.OnPlayerSpawned(OnGameInitialized);
+            PhotonNetwork.LocalPlayer.SetCustomProperties(new ExitGames.Client.Photon.Hashtable()
             {
-                var stringBuilder = new StringBuilder();
-                stringBuilder.AppendLine($"<color=yellow>==</color> Monke Click <color=yellow>==</color>");
-                stringBuilder.AppendLine(selectionHandler.GetOriginalBananaOSSelectionText(0, $"activity: {(active?"enabled":"disabled")}"));
-                stringBuilder.AppendLine(selectionHandler.GetOriginalBananaOSSelectionText(1, $"color: {colors[colorIndex].Item1}"));
-                return stringBuilder.ToString();
-            }
-
-            public override void OnButtonPressed(WatchButtonType buttonType)
-            {
-                switch (buttonType)
                 {
-                    case WatchButtonType.Up:
-                        selectionHandler.MoveSelectionUp();
-                        break;
-
-                    case WatchButtonType.Down:
-                        selectionHandler.MoveSelectionDown();
-                        break;
-
-                    case WatchButtonType.Enter:
-                    if (selectionHandler.currentIndex == 0) active = !active;
-                    else
-                    {
-                        colorIndex += 1;
-
-                        if (colorIndex > colors.Count - 1)
-                        {
-                            colorIndex = 0;
-                        }
-                        Plugin.RefreshColor(colors[colorIndex].Item2, Plugin.lineRenderer, colorIndex);
-                    }
-
-                        break;
-
-                    case WatchButtonType.Back:
-                        ReturnToMainMenu();
-                        break;
+                    PluginInfo.HashKey, PluginInfo.Version
                 }
+            });
+        }
+
+        void OnGameInitialized()
+        {
+            GameObject lineObj = new GameObject("RaycastLine");
+            lineRenderer = lineObj.AddComponent<LineRenderer>();
+            lineRenderer.positionCount = 2;
+            lineRenderer.startWidth = 0.01f;
+            lineRenderer.endWidth = 0.01f;
+            lineRenderer.material = new Material(Shader.Find("Sprites/Default"));
+            lineRenderer.enabled = false;
+        }
+
+        void FixedUpdate()
+        {
+            if (lineRenderer == null || GTPlayer.Instance == null) return;
+
+            // Hand config stuff
+            Transform handTransform = Plugin.useRightHand ? GTPlayer.Instance.rightControllerTransform : GTPlayer.Instance.leftControllerTransform;
+            bool grabPressed = Plugin.useRightHand ? ControllerInputPoller.instance.rightGrab : ControllerInputPoller.instance.leftGrab;
+            float indexTrigger = Plugin.useRightHand ? ControllerInputPoller.instance.rightControllerIndexFloat : ControllerInputPoller.instance.leftControllerIndexFloat;
+            Transform handTriggerCollider = Plugin.useRightHand ? GorillaTagger.Instance.rightHandTriggerCollider.transform : GorillaTagger.Instance.leftHandTriggerCollider.transform;
+
+            lineRenderer.SetPosition(0, handTransform.position);
+
+            if (grabPressed && Plugin.active)
+            {
+                lineRenderer.enabled = true;
+
+                var hits = Physics.RaycastAll(
+                    handTransform.position,
+                    handTransform.forward,
+                    Mathf.Infinity,
+                    ~(1 << LayerMask.NameToLayer("Gorilla Boundary"))
+                );
+                Array.Sort(hits, (a, b) => a.distance.CompareTo(b.distance));
+
+                foreach (var hit in hits)
+                {
+                    lineRenderer.SetPosition(1, hit.point);
+                    if (indexTrigger > 0.5f && hit.transform.gameObject.layer == 18)
+                    {
+                        handTriggerCollider.position = hit.point;
+                        break;
+                    }
+                }
+            }
+            else
+            {
+                lineRenderer.enabled = false;
+            }
+        }
+
+
+
+        public static void RefreshColor(Color newColor)
+        {
+            if (lineRenderer != null)
+            {
+                lineRenderer.startColor = newColor;
+                lineRenderer.endColor = newColor;
             }
         }
     }
+
+    // GorillaInfoWatch Screen implementation
+    [ShowOnHomeScreen]
+    internal class InfoWatchPage : GorillaInfoWatch.Models.InfoWatchScreen
+    {
+        public override string Title => "Monke Click";
+
+        public override ScreenContent GetContent()
+        {
+            var lines = new LineBuilder();
+
+            lines.Add($"Status: {(Plugin.active ? "Enabled" : "Disabled")}",
+                new List<Widget_Base> { new Widget_PushButton(OnToggleActive) });
+
+            lines.Add($"Color: {Plugin.colors[Plugin.colorIndex].Item1}",
+                new List<Widget_Base>
+                {
+                new Widget_PushButton(OnPreviousColor),
+                new Widget_PushButton(OnNextColor)
+                });
+
+            lines.Add($"Hand: {(Plugin.useRightHand ? "Right" : "Left")}",
+                new List<Widget_Base> { new Widget_PushButton(OnToggleHand) });
+
+            return lines;
+        }
+
+        private void OnToggleActive(object[] args)
+        {
+            Plugin.active = !Plugin.active;
+            SetContent();
+        }
+
+        private void OnPreviousColor(object[] args)
+        {
+            Plugin.colorIndex = (Plugin.colorIndex - 1 + Plugin.colors.Count) % Plugin.colors.Count;
+            Plugin.RefreshColor(Plugin.colors[Plugin.colorIndex].Item2);
+            SetContent();
+        }
+
+        private void OnNextColor(object[] args)
+        {
+            Plugin.colorIndex = (Plugin.colorIndex + 1) % Plugin.colors.Count;
+            Plugin.RefreshColor(Plugin.colors[Plugin.colorIndex].Item2);
+            SetContent();
+        }
+
+        private void OnToggleHand(object[] args)
+        {
+            Plugin.useRightHand = !Plugin.useRightHand;
+            SetContent();
+        }
+    }
+}
